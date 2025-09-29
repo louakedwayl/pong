6ecf0c296b96dcb00dd591dc54ec70be9c94430b
Wayl <louakedwayl@protonmail.com>
2025-09-29 21:52:46 +0200

    Add pause button and back button in game

--- frontend/src/game.ts
+++ frontend/src/game.ts
@@ -3,7 +3,7 @@ import {new_game_animation, updatePaddlePosition} from "./game_utils.js";
 async function startGame()
 {
     await  new_game_animation();
-updatePaddlePosition();
+    updatePaddlePosition();
 }
 
 startGame();
--- frontend/src/game_utils.ts
+++ frontend/src/game_utils.ts
@@ -1,5 +1,7 @@
 export const nav = document.querySelector("nav") as HTMLElement;
 export const button1 = document.querySelector("button.game") as HTMLButtonElement;
+export const button2 = document.querySelector("button.language") as HTMLButtonElement;
+
 export const playerPaddle = document.querySelector(".paddle.left") as HTMLElement;
 
 export let posY = 0;
@@ -12,6 +14,17 @@ export const keys = {
 
 export const maxY = 384; // Screen height
 export const minY = -384; //
+
+export const button3 = document.createElement("button") as HTMLButtonElement;
+export const button4 = document.createElement("button") as HTMLButtonElement;
+button3.classList.add("btn");
+button4.classList.add("btn");
+button3.textContent = "PAUSE";
+button3.style.paddingLeft = "26px";
+button3.style.paddingRight = "26px";
+button4.textContent = "BACK";
+button4.style.paddingLeft = "26px";
+button4.style.paddingRight = "26px";
 
 // Listen when a key was pressed
 document.addEventListener("keydown", (event) => {
@@ -28,27 +41,25 @@ if (event.key === "ArrowUp" || event.key === "ArrowDown") {
     event.preventDefault();
 }});
         
-
 export function new_game_animation()
 {
-  return new Promise<void>((resolve) => 
-  {
-      if (nav && button1 && playerPaddle)
-    {
-        button1.addEventListener("click", () => 
+    return new Promise<void>((resolve) => 
+        {
+            if (nav && button1 && playerPaddle)
             {
-                playerPaddle.classList.add("blink");
-        playerPaddle.addEventListener("animationend", () =>
-            {
-          playerPaddle.classList.remove("blink");
-          resolve ();
-        } , { once: true });
-    }, { once: true });
-    }
-  });
+                button1.addEventListener("click", () => 
+                {
+                    updateNav();
+                    playerPaddle.classList.add("blink");
+                    playerPaddle.addEventListener("animationend", () =>
+                    {
+                        playerPaddle.classList.remove("blink");
+                        resolve ();
+                    } , { once: true });
+                    }, { once: true });
+            }
+        });
 }
-
-
 
 // Fonction qui met à jour la position en continu
 export function updatePaddlePosition() 
@@ -63,4 +74,28 @@ export function updatePaddlePosition()
     playerPaddle.style.transform = `translateY(calc(-45px + ${posY}px))`;
     // Répéter à chaque frame
     requestAnimationFrame(updatePaddlePosition);
+}
+
+export function updateNav() 
+{
+        button1.style.display= "none";
+        button2.style.display= "none";
+        nav.appendChild(button3);
+        nav.appendChild(button4);   
+}
+
+export function handlerBack() 
+{
+    button4.addEventListener("click", () => 
+    {
+            // GERER le retour au menu
+    });
+}
+
+export function handlerPause() 
+{
+    button3.addEventListener("click", () => 
+    {
+        // GERER la pause
+    });
 }
