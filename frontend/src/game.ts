f9d860b5e4986f7a899afc52abe2171e56c94f44
Wayl Louaked <wlouaked@f0r9s19.clusters.42paris.fr>
2025-10-11 21:35:55 +0200

    Update opponent IA

--- frontend/src/game.ts
+++ frontend/src/game.ts
@@ -1,10 +1,12 @@
 import {handlerBack, handlerPause, new_game_animation} from "./game_utils.js";
 import { updatePaddlePosition } from "./paddle.js";
+import {updateOpponentPosition} from "./opponent.js";
 
 async function startGame()
 {
     await  new_game_animation();
     updatePaddlePosition();
+    updateOpponentPosition();
 }
 
 handlerBack();
---
+++ frontend/src/opponent.ts
@@ -0,0 +1,60 @@
+import {ball, pongScreen} from "./ball.js";
+import { pause } from "./game_utils.js";
+
+
+export const opponentPaddle = document.querySelector(".paddle.right") as HTMLElement;
+export let opponentAnimationId: number | null = null;
+export let opponentPosY = 0;
+
+const maxY = 384;
+const minY = -384;
+const opponentSpeed = 6;
+const offset = 23;
+let reactionDelay = 0;
+
+export function updateOpponentPosition()
+{
+    if (pause == true)
+        return;
+    
+    const screenRect = pongScreen.getBoundingClientRect();
+    const ballRect = ball.getBoundingClientRect();
+    const paddleRect = opponentPaddle.getBoundingClientRect();
+    const screenCenter = screenRect.left + screenRect.width / 2;
+    
+    if (ballRect.left >= screenCenter) {
+        reactionDelay++;
+        
+        if (reactionDelay >= 2) {
+            reactionDelay = 0;
+            
+            if (Math.random() > 0.12) {
+                const ballCenterY = ballRect.top + ballRect.height / 2;
+                const paddleCenterY = paddleRect.top + paddleRect.height / 2;
+                
+                if (ballCenterY < paddleCenterY - offset) {
+                    opponentPosY = Math.max(opponentPosY - opponentSpeed, minY);
+                }
+                else if (ballCenterY > paddleCenterY + offset) {
+                    opponentPosY = Math.min(opponentPosY + opponentSpeed, maxY);
+                }
+                opponentPaddle.style.transform = `translateY(calc(-45px + ${opponentPosY}px))`;
+            }
+        }
+    }
+    opponentAnimationId = requestAnimationFrame(updateOpponentPosition);
+}
+
+export function setOpponentAnimationId(id: number | null) {
+    opponentAnimationId = id;
+}
+
+export function getOpponentAnimationId() {
+    return opponentAnimationId;
+}
+
+export function setOpponentPosition(y : number)
+{
+    opponentPosY = y ;
+    opponentPaddle.style.transform = `translateY(calc(-45px + ${opponentPosY}px))`;
+}
